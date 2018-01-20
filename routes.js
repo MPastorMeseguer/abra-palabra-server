0e9cd7611c52a5cfadfb01259e02c396295ef7b2
vidocco <jack.mk.thibaut@gmail.com>
2018-01-20 19:53:18 +0100

    chore: game controller strucutre

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const jwtMiddleware = require('./helpers/jwt.helper').validateRequest;
 
 const auth = require('./controllers/auth');
 const card = require('./controllers/card');
-// const game = require('./controllers/game');
+const game = require('./controllers/game');
 
 router.post('/signup', auth.signup);
 router.post('/login', auth.login);
@@ -15,5 +15,7 @@ router.get('/token', auth.token);
 
 router.post('/card', jwtMiddleware, card.create);
 router.get('/card/:id', jwtMiddleware, card.get);
+
+router.post('/game', jwtMiddleware, game.init)
 
 module.exports = router;
